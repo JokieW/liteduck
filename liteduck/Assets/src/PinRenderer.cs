c976d15690e9cffc22b71efdee670a87f142d6a3
JokieW <gulliverjko@gmail.com>
2015-01-21 04:06:28 -0500

    Worked on the PinRenderer, it can now render on the gameObject's transform and render arbitrary image sizes.

--- liteduck/Assets/src/PinRenderer.cs
+++ liteduck/Assets/src/PinRenderer.cs
@@ -1,50 +1,98 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PinRenderer : MonoBehaviour 
 {
     public Mesh pin;
-    public Material mat;
     public Texture2D image;
     public float xspace = 0.17f;
     public float yspace = 0.15f;
 
+    private static Dictionary<Color, Material> _MatLibrary = new Dictionary<Color, Material>();
+    private static Material GetMaterial(Color color)
+    {
+        Material m;
+        if(!_MatLibrary.TryGetValue(color, out m))
+        {
+            m = new Material(Resources.Load<Material>("Objects/Pin/Materials/basePinMat"));
+            m.color = color;
+            _MatLibrary.Add(color, m);
+        }
+        return m;
+    }
+
 	// Use this for initialization
 	void Start () 
     {
-        for (int j = 0; j != 72; j++)
+        Render();
+    }
+
+    void Render()
+    {
+        //Create pixel DB for rendering
+        Color[] pixels = image.GetPixels(0,0,image.width, image.height);
+        Dictionary<Color, List<Vector2>> pixelDB = new Dictionary<Color, List<Vector2>>();
+
+        for (int i = 0; i != pixels.Length; i++)
         {
-            //MeshCombineUtility.MeshInstance[] mcu = new MeshCombineUtility.MeshInstance[114];
-            //int k = 0;
-            for (int i = 0; i != 114; i++)
+            List<Vector2> list;
+            if (!pixelDB.TryGetValue(pixels[i], out list))
             {
-                Color c = image.GetPixel(i, j);
-                if (c != Color.black)
-                {
-                    GameObject go = new GameObject(i+"_" + j);
-                    go.transform.position = new Vector3((j % 2 == 0 ? 0.085f : 0.0f) + (i * xspace), 0.0f, j * yspace);
-                    MeshFilter mf = go.AddComponent<MeshFilter>();
-                    mf.mesh = pin;
-                    MeshRenderer mr = go.AddComponent<MeshRenderer>();
-                    mr.material = new Material(mat);
-                    mr.material.color = c;
-                    mr.enabled = true;
-                    //mcu[k].mesh = pin;
-                    //mcu[k].subMeshIndex = 0;
-                    //mcu[k].transform = Matrix4x4.TRS(new Vector3((j % 2 == 0 ? 0.085f : 0.0f) + (i * xspace), 0.0f, j * yspace), Quaternion.identity, Vector3.one);
-                    //k++;
-                }
+                list = new List<Vector2>();
+                pixelDB.Add(pixels[i], list);
             }
-            /*GameObject go = new GameObject("Line_"+j);
-            go.transform.parent = gameObject.transform;
-            MeshFilter mf = go.AddComponent<MeshFilter>();
-            MeshRenderer mr = go.AddComponent<MeshRenderer>();
-            mf.mesh = MeshCombineUtility.Combine(mcu, false);
-            mf.mesh.RecalculateBounds();
-            mr.sharedMaterial = mat;
-            mr.enabled = true;*/
+            list.Add(new Vector2(i % image.width, i / image.width));
         }
-        
+
+        //Delete existing combined gameobjects
+        foreach (Transform child in transform)
+        {
+            if (child != transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+        //Create combined gameobjects
+        int vertCount = pin.vertexCount;
+        int maxPinPerObject = 65000 / vertCount;
+
+        foreach (KeyValuePair<Color, List<Vector2>> kvp in pixelDB)
+        {
+            GameObject go = null;
+            MeshFilter mf = null;
+            MeshCombineUtility.MeshInstance[] mcu = null;
+            int count = 0;
+            int k = 0;
+            foreach (Vector2 v2 in kvp.Value)
+            {
+                if (go == null || k == maxPinPerObject)
+                {
+                    if (go != null)
+                    {
+                        mf.sharedMesh = MeshCombineUtility.Combine(mcu, false);
+                        mf.sharedMesh.RecalculateBounds();
+                    }
+                    go = new GameObject("Render #" + (count / maxPinPerObject) + " of " + kvp.Key);
+                    go.transform.parent = transform;
+                    mf = go.AddComponent<MeshFilter>();
+                    mcu = new MeshCombineUtility.MeshInstance[maxPinPerObject];
+                    k = 0;
+                    MeshRenderer mr = go.AddComponent<MeshRenderer>();
+                    mr.sharedMaterial = GetMaterial(kvp.Key);
+
+                }
+                mcu[k].mesh = pin;
+                mcu[k].subMeshIndex = 0;
+                mcu[k].transform = Matrix4x4.TRS(new Vector3((v2.y % 2 == 0 ? 0.085f : 0.0f) + (v2.x * xspace), 0.0f, v2.y * yspace), Quaternion.identity, Vector3.one) * transform.localToWorldMatrix;
+                k++;
+                count++;
+            }
+            mf.sharedMesh = MeshCombineUtility.Combine(mcu, false);
+            mf.sharedMesh.RecalculateBounds();
+        }        
 	}
 	
 	// Update is called once per frame
