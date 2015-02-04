288e0059acb8bc238e92c8768303af8a19998c53
JokieW <gulliverjko@gmail.com>
2015-02-03 22:16:24 -0500

    Tiler

---
+++ liteduck/Assets/src/Tiler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Tiler : MonoBehaviour 
+{
+    public Texture2D tile;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void OnGUI () 
+    {
+        int divider = 1;
+        if (Event.current.shift)
+        {
+            divider = 2;
+        }
+        Vector3 pos = transform.position * divider;
+
+        transform.position = new Vector3(Mathf.Round(pos.x) / divider, Mathf.Round(pos.y) / divider, Mathf.Round(pos.z) / divider);
+	}
+}
