ce62fb1958fd0f792539e864f3e9cc29703b800e
JokieW <gulliverjko@gmail.com>
2015-02-12 22:51:56 -0500

    Ducker can control and run

--- liteduck/Assets/src/DuckerChanger.cs
+++ liteduck/Assets/src/DuckerChanger.cs
@@ -3,30 +3,80 @@ using System.Collections;
 
 public class DuckerChanger : MonoBehaviour 
 {
-    public Texture2D BlueTex;
-    public Texture2D RedTex;
-    public Texture2D YellowTex;
-    public MeshRenderer Renderer;
+    public Texture2D DuckSprite, CapeSprite;
+    public MeshRenderer DuckRender, CapeRender;
+    public int colorOffset = 0, frameOffset = -1;
+    public DuckerControls controls;
+    private Timer _frame;
+
+    private Vector3 _standingCenter = new Vector3(11.0f, 6.5f, 0.0f);
+
+    void Start()
+    {
+        _frame = new Timer(0.10f);
+        
+        
+    }
+
+    void Update()
+    {
+        int initialFrame = frameOffset;
+        if (controls.ducking)
+        {
+            frameOffset = 2;
+        }
+        else
+        {
+            if (_frame.Check())
+            {
+                if (frameOffset == 0)
+                {
+                    frameOffset = 1;
+                }
+                else
+                {
+                    frameOffset = 0;
+                }
+                _frame.Reset();
+            }
+        }
+        if (frameOffset != initialFrame)
+        {
+            Texture2D t = new Texture2D(20, 15);
+            t.filterMode = FilterMode.Point;
+            Color[] pixels = DuckSprite.GetPixels(frameOffset * 20, colorOffset * 15, 20, 15);
+            t.SetPixels(0, 0, 20, 15, pixels);
+            t.Apply();
+            DuckRender.sharedMaterial.mainTexture = t;
+            
+        }
+    }
+
+    void SetAs(GameColor color)
+    {
+        if (color == GameColor.Blue)
+        {
+            gameObject.layer = 12;
+            colorOffset = 2;
+        }
+        else if (color == GameColor.Red)
+        {
+            gameObject.layer = 11;
+            colorOffset = 1;
+        }
+        else if (color == GameColor.Yellow)
+        {
+            gameObject.layer = 13;
+            colorOffset = 0;
+        }
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         Door door = collider.gameObject.GetComponent<Door>();
         if(door != null)
         {
-            if (door.GameColor == GameColor.Blue)
-            {
-                gameObject.layer = 12;
-                Renderer.material.SetTexture(0, BlueTex);
-            }
-            else if (door.GameColor == GameColor.Red)
-            {
-                gameObject.layer = 11;
-                Renderer.material.SetTexture(0, RedTex);
-            }
-            else if (door.GameColor == GameColor.Yellow)
-            {
-                gameObject.layer = 13;
-                Renderer.material.SetTexture(0, YellowTex);
-            }
+            SetAs(door.GameColor);
         }
         else
         {
--- liteduck/Assets/src/DuckerControls.cs
+++ liteduck/Assets/src/DuckerControls.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class DuckerControls : MonoBehaviour 
 {
-    bool _grounded;
-    float ySpeed = 0.0f, yFallRate = 0.35f, yJumpRate = 0.35f;
-    float maxY = 8.0f, midY = 2.0f, minY = -2.0f;
-    Timer _jumpTime, _hoverTime;
+    public bool ducking = false;
+    private bool _grounded = true, _stoppedGoingUp = true;
+    private float ySpeed = 0.0f, yFallRate = 1f;
+    private float maxY = 4.0f, minY = -4.0f;
+    private Timer _jumpTime, _hoverTime;
 
     void Awake()
     {
@@ -16,45 +17,40 @@ public class DuckerControls : MonoBehaviour
 
 	void Update () 
     {
-        if (!_grounded)
+         if (_grounded)
+        {
+            ySpeed = 0.0f;
+        }
+        else if (!_grounded && _stoppedGoingUp)
         {
             if (ySpeed > minY)
             {
-                if (_hoverTime.Check())
-                {
-                    ySpeed -= yFallRate / 2;
-                }
-                else
-                {
-                    ySpeed -= yFallRate;
-                }
+                ySpeed -= yFallRate;
             }
         }
+        
         if (Input.GetKey(KeyCode.Space))
         {
             if (_grounded)
             {
                 _grounded = false;
-                ySpeed = midY;
+                ySpeed = maxY;
                 _jumpTime.Reset();
-                _jumpTime.Start(1.0f);
+                _jumpTime.Start(0.33f);
+                _stoppedGoingUp = false;
             }
-            if (!_jumpTime.Check())
+            if (_jumpTime.Check())
             {
-                ySpeed += yJumpRate;
+                _stoppedGoingUp = true;
             }
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
-            _hoverTime.Reset();
-            _hoverTime.Start(1.0f);
+            _stoppedGoingUp = true;
         }
-        else
-        {
-            ySpeed = 0.0f;
-        }
+        
 
-        transform.Translate(new Vector3(1.0f, ySpeed, 0.0f) * Time.deltaTime * 21.0f);
+        transform.Translate(new Vector3(2.0f, ySpeed, 0.0f) * Time.deltaTime * 21.0f);
         
 	}
 
@@ -66,10 +62,18 @@ public class DuckerControls : MonoBehaviour
             {
                 _grounded = true;
                 ySpeed = 0.0f;
+
+                transform.position = new Vector3(transform.position.x, 
+                    collision.collider.bounds.center.y + 2.0f, 
+                    transform.position.z);
+
                 break;
             }
         }
     }
-    
 
+    void OnCollisionExit(Collision collision)
+    {
+        _grounded = false;
+    }
 }
