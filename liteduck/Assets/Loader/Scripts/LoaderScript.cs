cc1db30ef27119ff296683aaf2e317e29c6f456c
Parnasse <Phallen52@HOTMAIL.COM>
2015-02-23 01:49:31 -0500

    Loader Version2.0 & TempAnimation

--- liteduck/Assets/Loader/Scripts/LoaderScript.cs
+++ liteduck/Assets/Loader/Scripts/LoaderScript.cs
@@ -3,51 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LoaderScript : MonoBehaviour {
+	//Obsolete version
+	//protected float ScreenFade = 1.0f; 
+	//public GameObject ScreenBlack = null;
+	//public Color ScreenFade = new Color(0.0f,0.0f,0.0f,1.0f);
+	//public float ScreenFade;
+	public Color CurrentFade = new Color(1,1,1,1);
 
-	protected float ScreenFade = 1.0f; 
-	private Material FadeMaterial = null;
-
-	//no pesky cursor when not needed, soo cool!! -SebF
 	void OnGUI()
 	{
 		Screen.showCursor = false;	
 	}
 
-	//post-processing on camera render, only works when attached/linked to camera. -SebF
-	void OnPostRender(){
-		{
-			if(!FadeMaterial) 
-			{
-				FadeMaterial = new Material( "Shader \"Hidden/CameraFade\" {" +
-				                            "Properties { _Color (\"Main Color\", Color) = (1,1,1,0) }" +
-				                            "SubShader {" +
-				                            "    Pass {" +
-				                            "        ZTest Always Cull Off ZWrite Off "+
-				                            "		 Blend SrcAlpha OneMinusSrcAlpha "+
-				                            "        Color [_Color]" +
-				                            "    }" +
-				                            "}" +
-				                            "}"
-				                            );
-			}
-
-			FadeMaterial.SetColor("_Color", new Color(0.0f,0.0f,0.0f, ScreenFade) );
-			GL.PushMatrix ();
-			GL.LoadOrtho ();
-
-			for (var i = 0; i < FadeMaterial.passCount; ++i) 
-			{
-				FadeMaterial.SetPass (i);
-				GL.Begin( GL.QUADS );
-				GL.Vertex3( 0, 0, 0.1f );
-				GL.Vertex3( 1, 0, 0.1f );
-				GL.Vertex3( 1, 1, 0.1f );
-				GL.Vertex3( 0, 1, 0.1f );
-				GL.End();
-			}
-			GL.PopMatrix (); 
-		}
-	}
 
 	private IEnumerator FadeIN()
 	{
@@ -60,7 +27,10 @@ public class LoaderScript : MonoBehaviour {
 		float timer= 3.0f;
 		while (timer > 0.0f) {
 			timer-=Time.deltaTime;
-			ScreenFade = timer/3.0f;
+			//ScreenFade = timer/3.0f;
+			Color alphaColor = gameObject.renderer.material.color;
+			alphaColor.a = timer/3.0f;
+			gameObject.renderer.material.color = alphaColor;
 			yield return null;
 		}
 		timer = 0.0f;
@@ -77,30 +47,26 @@ public class LoaderScript : MonoBehaviour {
 		float timer = 0.0f;
 		while (fadeTime > timer) {
 			timer+=Time.deltaTime;
-			ScreenFade = timer/fadeTime;
+			//ScreenFade = timer/fadeTime;
+			Color alphaColor = gameObject.renderer.material.color;
+			alphaColor.a = timer/fadeTime;
+			gameObject.renderer.material.color = alphaColor;
 			yield return null;
 		}
 		Invoke ("LoadMainMenu", 1.0f);
 	}
-
-
-
-	void Awake(){
-		ScreenFade = 1.0f;
-	}
+	
 
 	void Start () {
 		StartCoroutine(FadeIN());
+		//CurrentFade = gameObject.renderer.material.color.a;
 	}
 
 	private void LoadMainMenu(){
-		Application.LoadLevel("MainMenu");
+		Application.LoadLevel(1);
 	}
 
-	public float GetScreenFade(){
-		return ScreenFade;
-	}
-	public void SetScreenFade(float ScreenFader){
-		ScreenFade = ScreenFader;
+	void udpade(){
+		gameObject.renderer.material.color = CurrentFade;
 	}
 }
